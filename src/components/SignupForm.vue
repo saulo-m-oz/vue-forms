1322649e892860721cd9427d7cfc2f4c021bcca4
Caneco <saulo.m.ozorio@gmail.com>
2022-10-16 03:06:59 -0300

    Feature: Add Keyboard Events

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,13 +1,62 @@
 <template>
   <form>
     <label>Email</label>
-    <input type="email" required />
-  </form>
+    <input type="email" required v-model="email" />
+
+    <label>Password</label>
+    <input type="password" required v-model="password" />
+
+    <label>Role:</label>
+    <select v-model="role">
+        <option value="Developer">Developer</option>
+        <option value="Web Designer">Web Desginer</option>
+    </select>
+
+    <label>Skills</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">
+        {{skill}}
+    </div>
+
+    <div class="terms">
+        <input type="checkbox" required v-model="terms" />
+        <label>Accept terms and conditions</label>
+    </div>
+
+
+    
+</form>
+  <p>Email: {{email}} -- Password: {{password}}</p>
+  <p>Password: {{password}}</p>
+  <p>Role: {{role}}</p>
+  <p>Terms: {{terms}}</p>
+  <p>Names: {{names}}</p>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            email: null,
+            password: null,
+            role: "Web Designer",
+            terms: false,
+            names: [],
+            tempSkill: "",
+            skills: []
+        }
+    },
+    methods: {
+        addSkill(event){
+            if(event.keyCode === 188 && this.tempSkill){
+                const splitSkill = this.tempSkill.split(",")[0]
+                if(!this.skills.includes(splitSkill)){
+                    this.skills.push(splitSkill)
+                }
+                this.tempSkill = ""
+            }
+        }
+    }
 }
 </script>
 
@@ -29,13 +78,26 @@ export default {
         letter-spacing: 1px;
         font-weight: bold;
     }
-    input{
+    input, select{
         display: block;
         padding: 10px 6px;
         width: 100%;
         box-sizing: border-box;
         border: none;
         border-bottom: 1px solid #ddd;
-        color: #555;
+        color: #555; 
+    }
+    input[type="checkbox"]{
+        display: inline-block;
+        width: 16px;
+        margin: 0 10px 0 0;
+        position: relative;
+        top: 2px;
+    }
+    .pill{
+        padding: 10px;
+        letter-spacing: 1.5px;
+        border-radius: 10px;
+        border: 1px solid #ddd;
     }
 </style>
